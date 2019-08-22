8824153d87fd8102181a2cd4b0d2f608b59ec6dc
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-08-21 23:07:46 -0700

    added button to form.js and updated css

--- src/components/form.js
+++ src/components/form.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 
 const Form = (props) => {
-
+const [person, setPerson] = useState({name: "", email: "", role: ""}); 
 return (
 <form>
-<input placeholder='name'/>
-<input placeholder='email'/>
-<input placeholder='role'/>
+<input placeholder='name' value={person.name} onChange={event => { setPerson({...person, name: event.target.value});}} />
+<input placeholder='email' value={person.email} onChange={event => { setPerson({...person, email: event.target.value});}} />
+<input placeholder='role' value={person.role} onChange={event => { setPerson({...person, role: event.target.value});}}/>
+
+<button type='submit'>Add Member</button>
+
 </form>
 
 
