6ba150c0873fb7b6216e7e69cac15da845b36ef0
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-08-21 21:23:39 -0700

    team builder card component created

---
+++ src/components/card.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+
+{ name: "", email: "", role: "" }; 
+
+const Card = (props) => {
+    const person = props.person; 
+    return (
+        <div>
+<div>person.name</div>
+<div>person.email</div>
+<div>person.role</div>
+        </div>
+    )
+};
+
+
+export default Card; 
